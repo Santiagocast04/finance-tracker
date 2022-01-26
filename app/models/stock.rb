d98aba66b7bdf7ec6d57a3daac396e4e1ca8205e
Fabio Andrés Castañeda <fcastaneda@picap.co>
2022-01-26 11:20:30 -0500

    show stock symbol,name and price. and adding ajax request to don't reload the hole page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,10 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_sym)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    return client.price(ticker_sym)
+    begin
+      new(ticker: ticker_sym, name:client.company(ticker_sym).company_name, last_price: client.price(ticker_sym))
+    rescue => exception
+      return nil
+    end
   end
 end
